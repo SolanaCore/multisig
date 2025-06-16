3571a682a1e21302cbe103af2ac1d7b0afe115be
Dhruv Khandelwal <artificialintelligencehub35@gmail.com>
2025-06-16 23:09:22 +0530

    feat: Updated the enum ErrorCode

--- multisig/programs/multisig/src/error.rs
+++ multisig/programs/multisig/src/error.rs
@@ -2,6 +2,12 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum ErrorCode {
-    #[msg("Custom error message")]
-    CustomError,
+    #[msg("Threshold must be greater than zero and less than or equal to number of owners")]
+    InvalidThreshold,
+    #[msg("The owner is not part of the multisig group")]
+    InvalidOwner,
+    #[msg("Not enough owners signed this transaction.")]
+    InsufficientSigners,
+    #[msg("Transaction is already executed")]
+    TransactionAlreadyExecuted,
 }
