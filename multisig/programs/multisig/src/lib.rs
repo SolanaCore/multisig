80a28c9af1043041480247622a1919d56995182e
DhruvWebDev <artificialintelligencehub35@gmail.com>
2025-06-16 18:59:05 +0000

    feat: security.txt in the solana program

--- multisig/programs/multisig/src/lib.rs
+++ multisig/programs/multisig/src/lib.rs
@@ -1,3 +1,4 @@
+#[allow(unused)]
 pub mod constants;
 pub mod error;
 pub mod instructions;
@@ -9,12 +10,20 @@ pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
+#[cfg(not(feature = "no-entrypoint"))]
+solana_security_txt::security_txt! {
+    name: "multisig",
+    project_url: "https://github.com/SolanaCore/multisig",
+    contacts:"mailto:artificialintelligencehub35@gmail.com",
+    source_code: "https://github.com/SolanaCore/multisig",
+    preferred_languages: "en"
+}
+
 declare_id!("AwhGP9QqsN2JAaS2XyYo2PeC2EAvkCExYLd5Mfuq1GaQ");
 
 #[program]
 pub mod multisig {
     use super::*;
-
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         initialize::handler(ctx)
     }
